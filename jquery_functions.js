f65a1f54ced6baec0dc906f4c8704e120a289d60
Konrad Gruszczynski <wierzba41@gmail.com>
2022-04-09 22:57:09 +0200

    API call now differs based on user choices

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,42 +1,64 @@
 //$(selector).action()
 $(document).ready(function(){
-    
-    $(".startButton").click(startButtonAnimation);
+    $(".startButton").click(function(){
+        callAPI();
+        startButtonAnimation();
+    });
 
-    $.get("https://opentdb.com/api.php?amount=1&type=multiple", function(data){
-        //alert("Data: " + JSON.stringify(data.results));
+    /*$.get("https://opentdb.com/api.php?amount=1&type=multiple", function(data){
         data.results.forEach(result => {
-            //alert(JSON.stringify(result));
-            
             var answers = result.incorrect_answers;
             answers.push(result.correct_answer);
             shuffleArray(answers);
-            //$(".question").html(answers);
             for (let i = 0; i < answers.length; i++) {
                 $(".multipleChoice").append("<div class='multiAnswer" + i + "'>" + answers[i] + "</div>");
                 if (answers[i] == result.correct_answer){
-                    $(".multiAnswer" + i).css("color","green");
+                    $(".multiAnswer" + i.css("color","green"));
                 }
             }
         });
-
-        
         }
-    );
+    );*/
 });
+
+
+
+function callAPI(){
+    let APIurl = "https://opentdb.com/api.php?";
+    $('.selectChoice').map(function(){
+        if(this.value){
+            APIurl = APIurl.concat(this.name + "=" + this.value + "&");
+        }
+    });
+
+    let radioValue = $('input[name="type"]:checked').val();
+    if (radioValue){
+        let radioName = $('input[name="type"]:checked').attr('name');
+        APIurl = APIurl.concat(radioName + "=" + radioValue);
+    } else {
+        APIurl = APIurl.slice(0, -1);
+    }
+
+    //alert(APIurl);
+
+    $.get(APIurl, function(data){
+        alert(JSON.stringify(data));
+    });
+}
 
 
 function startButtonAnimation(){
     $(".startButtonText").html( $(".startButton").html() );
-        $(".startButton").html("");
-        $(".startButtonText").fadeOut({duration:1000, queue:false});
-        $(".startButton").fadeOut({duration:1000, queue:false});
-        $(".startButtonText").animate({"font-size": "150px"}, 1000);
-        this.disabled = true;
+    $(".startButton").html("");
+    $(".startButtonText").fadeOut({duration:1000, queue:false});
+    $(".startButton").fadeOut({duration:1000, queue:false});
+    $(".startButtonText").animate({"font-size": "150px"}, 1000);
+    this.disabled = true;
 }
-/*function gameContent() {
+
+function showGameContent(){
     
-}*/
+}
 
 // copied from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(array) {
